5bc15a872d67f3b31c2dd69131006d7e2235f86a
root <nancymuthee>
2022-06-22 11:36:38 -0700

    edits

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
 class Square:
-'''(python3 -c 'print(__import__("my_module").MyClass.__doc__)')
-    printing an empty class
-'''
+    """This is a python docstring!!!"""
     pass
