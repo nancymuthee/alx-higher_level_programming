22c87c6618154bfef2ada904e50f329e8457ea8b
root <nancymuthee>
2022-06-27 08:32:59 -0700

    rectangle formula's

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-class rectangle
-'''
-printing an empty file
-and adding rectangle class
-'''
-pass
-'''
-any doc as long as it's
-empty you can do this.
-how to print empty doc
-'''
+"""
+A rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle functions and data
+    """
+
+    pass
