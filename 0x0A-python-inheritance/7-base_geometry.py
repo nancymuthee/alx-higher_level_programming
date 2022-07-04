879e89a71b0167f5566ad4c3dadd591546454727
root <nancymuthee>
2022-07-04 13:57:27 -0700

    edits

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Integer validator """
+"""BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """ Class BaseGeometry used to print raise errors. """
+    """Contains functions area(), integer_validator()
+    """
+
     def area(self):
-        """ raise an Exception """
+        """Function not implemented yet
+        """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Function that validates `value`
         """
-        Method used to validate "Value"
-        Args:
-            name:
-            vale:
-        Raises:
-            TypeError: if value is not an int
-            ValueError:if value is less or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(name + " must be greater than 0")
