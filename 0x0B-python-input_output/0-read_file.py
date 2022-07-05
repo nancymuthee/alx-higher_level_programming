25363c864f63229cc414806ffd02e180fbbf1860
root <nancymuthee>
2022-07-05 14:56:35 -0700

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """read_file
 """
 
@@ -7,5 +8,5 @@ def read_file(filename=""):
     """Takes in str filename to read it
     """
 
-    with open(filename, encoding="utf-8") as readFile:
-        print(readFile.read(), end='')
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/10-students.py
+++ 0x0B-python-input_output/10-students.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Student
-"""
+"""method for student creation"""
 
 
 class Student:
-    """Contains student data
-    """
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -13,16 +12,10 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary of Student with conditions to filter
-        """
-
-        if attrs == None or type(attrs) != list:
+        if attrs is None:
             return self.__dict__
-        else:
-            temp = {}
-            for elem in attrs:
-                if type(elem) != str:
-                    return self.__dict__
-                if elem in self.__dict__.keys():
-                    temp[elem] = self.__dict__[elem]
-            return temp
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return 
