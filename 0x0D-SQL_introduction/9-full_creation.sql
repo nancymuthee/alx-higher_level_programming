5beec29fa7f4dde6731ef9320dff32c6a1dbb75c
root <nancymuthee>
2022-08-10 15:18:32 -0700

    edit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,11 @@
-CREATE TABLE second_table
-(
-  ID int,
-  Name varchar(20),
-  Score int,
-);
+/*Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows*/
 
-INSERT INTO second_table (id INT, name VARCHAR(256), score INT) 
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(20), score INT);
+
+INSERT INTO second_table (id, name, score) 
 VALUES 
-      (id = 1, name = “John”, score = 10),
-      (id = 2, name = “Alex”, score = 3),
-      (id = 3, name = “Bob”, score = 14),
-      (id = 4, name = “George”, score = 8);
+      (1, "John", 10),
+      (2, "Alex", 3),
+      (3, "Bob", 14),
+      (4, "George", 8);
 
