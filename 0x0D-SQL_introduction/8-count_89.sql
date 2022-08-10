e035ad74833a652cb3091787ff8585383bd18aa2
root <nancymuthee>
2022-08-10 11:42:45 -0700

    mysql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(89) AS total_records FROM first_table
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;                                              
+
+GRANT SELECT, INSERT, CREATE, ALTER, DROP, LOCK TABLES, CREATE TEMPORARY TABLES, DELETE, UPDATE, EXECUTE ON hbtn_0d_2.* TO'user_0d_2';
+
+
