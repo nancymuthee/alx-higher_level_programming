40d30d328a41303264180f1b86603283667de5fd
Nancy Muthee <nancy.mwendwa.muthee@gmail.com>
2022-09-06 15:49:37 +0300

    javascript

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// JS First constant, first print
+
+const myVar = 'JavaScript is amazing';
+    console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+//JS 3languages
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
